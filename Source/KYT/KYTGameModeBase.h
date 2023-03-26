f2a1e1c1e4a9d9ed5c0a1e8a785b45e6bb47c008
YoungTaeKim <youngtae6302@naver.com>
2023-03-26 20:18:28 +0900

    Initial commit

---
+++ Source/KYT/KYTGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KYTGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KYT_API AKYTGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
